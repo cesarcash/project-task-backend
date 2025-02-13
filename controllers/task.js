12b7940e33ba7f2d4d84995866566d3173c8d7c1
cesar salazar <cesarcash5@gmail.com>
2025-02-13 00:08:37 -0600

    creacion de rutas

---
+++ controllers/task.js
@@ -0,0 +1,17 @@
+const getTasks = async (req, res) => {
+
+}
+
+const createTask = async (req, res) => {
+
+}
+
+const updateTask = async (req, res) => {
+
+}
+
+const deleteTask = async (req, res) => {
+
+}
+
+module.exports = { getTasks, createTask, updateTask, deleteTask };
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const { HttpStatus, HttpResponseMessage } = require('../enums/http');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const AuthError = require('../middleware/errors/AuthError');
 const NotFoundError = require('../middleware/errors/NotFoundError');
 const BadRequestError = require('../middleware/errors/BadRequestError');
@@ -51,6 +52,53 @@ const updateUser = async (req, res, next) => {
 
 }
 
+const createUser = async (req, res, next) => {
+
+  try{
+
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+      throw new BadRequestError(HttpResponseMessage.BAD_REQUEST);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({name, email, password: hashedPassword});
+    res.status(HttpStatus.CREATED).send({data: {_id: user._id, name: user.name, email: user.email}});
+
+  }catch(e){
+    next(e);
+  }
+
+}
+
+const login = async (req, res, next) => {
+
+  try{
+
+    const {email, password} = req.body;
+    if(!email || !password){
+      throw new BadRequestError(HttpResponseMessage.BAD_REQUEST);
+    }
+
+    const user = await User.findOne({email}).select('+password');
+    if(!user){
+      throw new AuthError(HttpResponseMessage.UNAUTHORIZED);
+    }
+
+    const matched = await bcrypt.compare(password, user.password);
+    if(!matched){
+      throw new AuthError(HttpResponseMessage.UNAUTHORIZED);
+    }
+
+    const token = jwt.sign({id: user._id}, NODE_ENV === 'production' ? JWT_SECRET : 'secreto', {expiresIn: '7d'});
+    res.status(HttpStatus.OK).send({token});
+
+  }catch(e){
+    next(e);
+  }
+
+}
+
 module.exports = {
-  getUser, updateUser
+  getUser, updateUser, createUser, login
 }
