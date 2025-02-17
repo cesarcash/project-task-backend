8d655e957c610a5e879311d1dd40e13658ec6c8a
cesar salazar <cesarcash5@gmail.com>
2025-02-16 21:08:58 -0600

    cors

--- app.js
+++ app.js
@@ -31,6 +31,25 @@ app.use(cors());
 app.options('*',cors());
 app.use(requestLogger);
 
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS.join(','));
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
 app.post('/signin', validateLoginUser, login);
 app.post('/signup', validateCreateUser, createUser);
 
